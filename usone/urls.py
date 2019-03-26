d4efe00eb0cc7ea45e3a4f054846117d250dd85b
sonya <code.sonya@gmail.com>
2019-03-26 09:43:42 +0900

    modify scheduler & layout

--- usone/urls.py
+++ usone/urls.py
@@ -17,10 +17,18 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^hr/', include('hr.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^dashboard/', include('dashboard.urls')),
+    url(r'^scheduler/', include('scheduler.urls')),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
