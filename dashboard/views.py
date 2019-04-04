a66a2a63e7dffa5b23eb0662ec400a005b084889
sonya <code.sonya@gmail.com>
2019-04-04 17:26:23 +0900

    modify dashboard files

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,21 +3,45 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+import pandas as pd
+from datetime import datetime, timedelta
+import requests
+from django.db.models import Q
+from dateutil.relativedelta import relativedelta
 
-#
-# def index(request):
-#     template = loader.get_template('index.html')
-#     context = {
-#         'latest_question_list': "index",
-#     }
-#     return HttpResponse(template.render(context, request))
-#
+from scheduler.models import Eventday
+from service.models import Servicereport, Vacation
+from django.contrib.auth.models import User
+from django.http import QueryDict
+from django.db.models import FloatField
+from django.db.models import Sum
+
+
 
 def dashboard(request):
-    if request.user.id:
-        template = loader.get_template('dashboard/index.html')
+    if request.user.id: #로그인 유무
+        template = loader.get_template('dashboard/dashboard.html')
+
+        #기간 설정 했을 경우
+        if request.method=="POST":
+
+            startdate=request.POST['startdate']
+            enddate = request.POST['startdate']
+
+        #default : 주 단위 (월~일)
+        else:
+            today = datetime.today()
+            startdate = today - timedelta(days=today.weekday())
+            enddate = startdate + timedelta(days=6)
+            print(startdate.date(),enddate.date())
+
+        customer_support_time= Servicereport.objects.values('companyName')\
+                                                    .filter(Q(serviceDate__gte=startdate)&
+                                                            Q(serviceDate__lte=enddate))\
+                                                    .annotate(sum_supportTime=Sum('serviceHour'))
+
         context = {
-            'latest_question_list': "index",
+            'customer_support_time': customer_support_time,
         }
         return HttpResponse(template.render(context, request))
     else:
