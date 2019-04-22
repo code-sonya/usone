398013b1f6aebba32f04c5052bd8800b11765a99
sonya <code.sonya@gmail.com>
2019-04-22 16:59:16 +0900

    modified static folder

--- signature/static/signature/js/app.js
+++ signature/static/signature/js/app.js
@@ -56,6 +56,7 @@ function download(dataURL, filename) {
     form_data.append('file', blob);
 
     signatureUrl = '/signature/saveimg/'+ filename + "/";
+    // console.log(signatureUrl);
     $.ajax({
         url: signatureUrl,
         method: 'POST',
