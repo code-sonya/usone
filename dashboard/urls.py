13973d8edd2906f0af6cbc975b02ec3397748483
sonya <code.sonya@gmail.com>
2019-04-30 14:24:26 +0900

    excel,csv download

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'dashboard'
+
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^dashboardreport/$', views.dashboardreport, name='dashboardreport'),
+    url(r'^overhour/$', views.over_hour, name='over_hour'),
+    url(r'^overjson/$', views.over_asjson, name='over_ajax_url'),
+    url(r'^filterjson/$', views.filter_asjson, name='filter_ajax_url'),
 ]
