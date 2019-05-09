b192fa9352bc6e8ced8ce19c1172f55206802a9c
sonya <code.sonya@gmail.com>
2019-05-09 14:23:11 +0900

    modified contract dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    url(r'^$', views.dashboard, name='dashboard'),
-    url(r'^dashboardreport/$', views.dashboardreport, name='dashboardreport'),
+    url(r'^service/$', views.dashboard_service, name='dashboardservice'),
+    url(r'^opportunity/$', views.dashboard_opportunity, name='dashboardopportunity'),
+    url(r'^sales/$', views.dashboard_sales, name='dashboardsales'),
+    url(r'^contract/$', views.dashboard_contract, name='dashboardcontract'),
     url(r'^overhour/$', views.over_hour, name='over_hour'),
     url(r'^overjson/$', views.over_asjson, name='over_ajax_url'),
     url(r'^filterjson/$', views.filter_asjson, name='filter_ajax_url'),
--- dashboard/views.py
+++ dashboard/views.py
@@ -16,8 +16,8 @@ from service.models import Servicereport
 
 
 @login_required
-def dashboard(request):
-    template = loader.get_template('dashboard/dashboard.html')
+def dashboard_service(request):
+    template = loader.get_template('dashboard/dashboardservice.html')
 
     # 기간 설정 했을 경우
     if request.method == "POST":
@@ -107,76 +107,6 @@ def dashboard(request):
 
 
 @login_required
-def dashboardreport(request):
-    template = loader.get_template('dashboard/dashboard_report.html')
-
-    # 기간 설정 했을 경우
-    if request.method == "POST":
-
-        startdate = request.POST['startdate']
-        enddate = request.POST['enddate']
-        print(startdate, enddate)
-
-    # default : 주 단위 (월~일)
-    else:
-        today = datetime.today()
-        startdate = (today - timedelta(days=today.weekday())).date()
-        enddate = startdate + timedelta(days=6)
-
-    # 전체 지원 통계
-    all_support_data = Servicereport.objects.filter(Q(serviceDate__gte=startdate) &
-                                                    Q(serviceDate__lte=enddate) &
-                                                    Q(empDeptName=request.user.employee.empDeptName))
-
-    all_support_time = Servicereport.objects.filter(Q(serviceDate__gte=startdate) &
-                                                    Q(serviceDate__lte=enddate) &
-                                                    Q(empDeptName=request.user.employee.empDeptName)) \
-        .aggregate(Sum('serviceHour'), Count('serviceHour'))
-
-    all_support_Overtime = Servicereport.objects.filter(Q(serviceDate__gte=startdate) &
-                                                        Q(serviceDate__lte=enddate) &
-                                                        Q(empDeptName=request.user.employee.empDeptName)) \
-        .aggregate(Sum('serviceOverHour'))
-
-    # 고객사별 지원통계
-    customer_support_time = Servicereport.objects.values('companyName') \
-        .filter(Q(serviceDate__gte=startdate) &
-                Q(serviceDate__lte=enddate) &
-                Q(empDeptName=request.user.employee.empDeptName)) \
-        .annotate(sum_supportTime=Sum('serviceHour'))
-
-    # 엔지니어별 지원통계
-    emp_support_time = Servicereport.objects.values('empName') \
-        .filter(Q(serviceDate__gte=startdate) &
-                Q(serviceDate__lte=enddate) &
-                Q(empDeptName=request.user.employee.empDeptName)) \
-        .annotate(sum_supportTime=Sum('serviceHour')).annotate(sum_supportCount=Count('empName')).annotate(sum_overTime=Sum('serviceOverHour'))
-
-    # 타입별 지원통계
-    type_support_time = Servicereport.objects.values('serviceType') \
-        .filter(Q(serviceDate__gte=startdate) &
-                Q(serviceDate__lte=enddate) &
-                Q(empDeptName=request.user.employee.empDeptName)) \
-        .annotate(sum_supportTime=Sum('serviceHour')).order_by('serviceType')
-
-    type_count = [i for i in range(len(type_support_time))]
-    multi_type = zip(type_support_time, type_count)
-
-    context = {
-        'startdate': startdate,
-        'enddate': enddate,
-        'customer_support_time': customer_support_time,
-        'emp_support_time': emp_support_time,
-        'type_support_time': type_support_time,
-        'multi_type': multi_type,
-        'all_support_data': all_support_data,
-        'all_support_time': all_support_time,
-        'all_support_Overtime': all_support_Overtime
-    }
-    return HttpResponse(template.render(context, request))
-
-
-@login_required
 def over_hour(request):
     template = loader.get_template('dashboard/overhourlist.html')
 
@@ -216,4 +146,25 @@ def filter_asjson(request):
     overHourlist = overHour.values('serviceStartDatetime', 'serviceEndDatetime', 'empId__empName', 'empId__empDeptName', 'companyName','serviceOverHour')
     structure = json.dumps(list(overHourlist), cls=DjangoJSONEncoder)
     return HttpResponse(structure, content_type='application/json')
+
+@login_required
+def dashboard_opportunity(request):
+    template = loader.get_template('dashboard/dashboardopportunity.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def dashboard_sales(request):
+    template = loader.get_template('dashboard/dashboardsales.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def dashboard_contract(request):
+    template = loader.get_template('dashboard/dashboardcontract.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
 
