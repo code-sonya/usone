d1d9b19e14eb506ce45caf2a23eaaedd6c3edbf7
sonya <code.sonya@gmail.com>
2019-12-17 10:35:26 +0900

    modified layout & model

---
+++ logs/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from hr.models import Employee
+from sales.models import Contract
+
+
+class DownloadLog(models.Model):
+    downloadLogId = models.AutoField(primary_key=True)
+    empId = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    contractId = models.ForeignKey(Contract, on_delete=models.SET_NULL, null=True, blank=True)
+    downloadDatetime = models.DateTimeField()
+    downloadUrl = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.downloadLogId)
+
+class OrderLog(models.Model):
+    orderLogId = models.AutoField(primary_key=True)
+    empId = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    toEmail = models.CharField(max_length=50)
+    contractId = models.ForeignKey(Contract, on_delete=models.SET_NULL, null=True, blank=True)
+    orderDatetime = models.DateTimeField()
+    orderUrl = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.orderLogId)
+
+
+class ApprovalLog(models.Model):
+    approvalLogId = models.AutoField(primary_key=True)
+    empId = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    toEmail = models.CharField(max_length=50)
+    contractId = models.ForeignKey(Contract, on_delete=models.SET_NULL, null=True, blank=True)
+    approvalDatetime = models.DateTimeField()
+    approvalUrl = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.approvalLogId)
--- usone/settings/base.py
+++ usone/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'sales',
     'extrapay',
     'approval',
+    'logs',
 ]
 
 MIDDLEWARE = [
