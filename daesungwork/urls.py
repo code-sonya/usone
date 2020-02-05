d9c1ea9a26d40511a450b293e59c9531859129d5
sonya <code.sonya@gmail.com>
2020-02-05 13:17:40 +0900

    cneter manager done

--- daesungwork/urls.py
+++ daesungwork/urls.py
@@ -6,7 +6,9 @@ app_name = 'daesungwork'
 urlpatterns = [
     url(r'^showcentermanagers/$', views.show_centermanagers, name='showcentermanagers'),
     url(r'^postmanager/$', views.post_manager, name='postmanager'),
+    url(r'^modifycentermanager/(?P<centerManagerId>.+)/$', views.modify_centermanager, name='modifycentermanager'),
     url(r'^centerasjson/$', views.center_asjson, name='centerasjson'),
     url(r'^centermanagerasjson/$', views.centermanager_asjson, name='centermanagerasjson'),
     url(r'^viewcentermanager/(?P<centerManagerId>.+)/$', views.view_centermanager, name='viewcentermanager'),
+    url(r'^deletecentermanager/(?P<centerManagerId>.+)/$', views.delete_centermanager, name='deletecentermanager'),
 ]
--- daesungwork/views.py
+++ daesungwork/views.py
@@ -17,7 +17,7 @@ import datetime
 
 # Create your views here.
 
-
+@login_required
 def show_centermanagers(request):
     template = loader.get_template('daesungwork/showcentermanagers.html')
     today = datetime.datetime.today()
@@ -29,6 +29,7 @@ def show_centermanagers(request):
     context = {
         'centermanager': centermanager,
         'centermanageremps': centermanageremps,
+        'today': today,
 
     }
     return HttpResponse(template.render(context, request))
@@ -53,7 +54,7 @@ def post_manager(request):
                     empId=Employee.objects.get(empId=item["empId"]),
                     manageArea=Center.objects.get(centerName=item["manageArea"]),
                     additionalArea=item["additionalArea"],
-                    cleanupArea=Center.objects.filter(centerName=item["cleanupArea"]).first(),
+                    cleanupArea=Center.objects.get(centerName=item["cleanupArea"]),
                 )
 
             return redirect('daesungwork:showcentermanagers')
@@ -68,12 +69,68 @@ def post_manager(request):
 
 @login_required
 @csrf_exempt
+def modify_centermanager(request, centerManagerId):
+    template = loader.get_template('daesungwork/postmanager.html')
+    centerManagerInstance = CenterManager.objects.get(centerManagerId=centerManagerId)
+    if request.method == 'POST':
+        form = CenterManagerForm(request.POST, instance=centerManagerInstance)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.writeEmp = Employee.objects.get(empId=request.user.employee.empId)
+            post.save()
+
+            jsonManagerEmp = json.loads(request.POST['jsonManagerEmp'])
+            managerEmpId = list(i[0] for i in CenterManagerEmp.objects.filter(centerManagerId=centerManagerId).values_list('managerId'))
+            jsonManagerEmpId = []
+            for item in jsonManagerEmp:
+                if item['centerManagerEmpId'] == '추가':
+                    CenterManagerEmp.objects.create(
+                        centerManagerId=post,
+                        empId=Employee.objects.get(empId=item["empId"]),
+                        manageArea=Center.objects.get(centerName=item["manageArea"]),
+                        additionalArea=item["additionalArea"],
+                        cleanupArea=Center.objects.filter(centerName=item["cleanupArea"]).first(),
+                    )
+                else:
+                    managerEmpInstance = CenterManagerEmp.objects.get(managerId=int(item['centerManagerEmpId']))
+                    managerEmpInstance.centerManagerId = post
+                    managerEmpInstance.empId = Employee.objects.get(empId=item["empId"])
+                    managerEmpInstance.manageArea = Center.objects.get(centerName=item["manageArea"])
+                    managerEmpInstance.additionalArea = item["additionalArea"]
+                    managerEmpInstance.cleanupArea = Center.objects.filter(centerName=item["cleanupArea"]).first()
+                    managerEmpInstance.save()
+                    jsonManagerEmpId.append(int(item['centerManagerEmpId']))
+
+            delManagerEmpId = list(set(managerEmpId) - set(jsonManagerEmpId))
+            if delManagerEmpId:
+                for Id in delManagerEmpId:
+                    CenterManagerEmp.objects.filter(managerId=Id).delete()
+            return redirect('daesungwork:showcentermanagers')
+        else:
+            return HttpResponse('유효하지 않은 형식입니다.')
+
+    else:
+        form = CenterManagerForm(instance=centerManagerInstance)
+        centerManagerEmps = CenterManagerEmp.objects.filter(centerManagerId=centerManagerId)
+        context = {
+            'centerManagerId': centerManagerId,
+            'form': form,
+            'centerManagerEmps': centerManagerEmps,
+        }
+
+    return HttpResponse(template.render(context, request))
+
+
+@login_required
+@csrf_exempt
 def center_asjson(request):
     centerManagerId = request.POST['centerManagerId']
     if centerManagerId:
         centers = Center.objects.all().values('centerName').order_by('centerName')
         employees = Employee.objects.filter(empStatus='Y').values('empName', 'empId').order_by('empId')
-        centerManagerEmps = CenterManagerEmp.objects.filter(centerManagerId=centerManagerId).values('')
+        centerManagerEmps = CenterManagerEmp.objects.filter(centerManagerId=centerManagerId)\
+            .values('empId', 'empId__empName', 'manageArea__centerName', 'additionalArea', 'cleanupArea__centerName')
         jsonList = []
         jsonList.append(list(centers))
         jsonList.append(list(employees))
@@ -93,9 +150,11 @@ def center_asjson(request):
 @login_required
 @csrf_exempt
 def centermanager_asjson(request):
-    centermanagers = CenterManager.objects.filter(centerManagerStatus='Y').values('centerManagerId', 'writeEmp__empName', 'startDate', 'endDate', 'createdDatetime').order_by('centerManagerId')
+    centermanagers = CenterManager.objects.filter(centerManagerStatus='Y')\
+        .values('centerManagerId', 'writeEmp__empName', 'startDate', 'endDate', 'createdDatetime').order_by('centerManagerId')
     structure = json.dumps(list(centermanagers), cls=DjangoJSONEncoder)
     return HttpResponse(structure, content_type='application/json')
+
 
 @login_required
 @csrf_exempt
@@ -108,3 +167,11 @@ def view_centermanager(request, centerManagerId):
         'centermanageremps': centermanageremps,
     }
     return HttpResponse(template.render(context, request))
+
+
+@login_required
+@csrf_exempt
+def delete_centermanager(request, centerManagerId):
+    centermanager = CenterManager.objects.get(centerManagerId=centerManagerId)
+    centermanager.delete()
+    return redirect('daesungwork:showcentermanagers')
